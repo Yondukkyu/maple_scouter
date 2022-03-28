f858bb5a4f00a173ba20ef4963a33b660b0f1cd7
devitaro <dochi2008@naver.com>
2022-03-28 23:32:33 +0900

    major commit

--- src/app/scouter/containers/auxiliary-table/auxiliary-table.component.ts
+++ src/app/scouter/containers/auxiliary-table/auxiliary-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-auxiliary-table',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auxiliary-table.component.scss']
 })
 export class AuxiliaryTableComponent implements OnInit {
+
+  @Input() auxiliary_table_list :number[] = [];
+  @Input() auxiliary_table :number[] = [];
 
   constructor() { }
 
