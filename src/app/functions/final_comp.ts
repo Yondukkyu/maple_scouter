6d83d3584469467fd1f5ac6ca771aa7b8654bae0
devitaro <dochi2008@naver.com>
2022-04-05 14:07:43 +0900

    error fix

---
+++ src/app/functions/final_comp.ts
@@ -0,0 +1,26 @@
+export function infinity_final_calc(coolDown:number)
+{
+
+}
+
+export function equilibrium_final_calc()
+{
+
+}
+
+export function CR_final_calc()
+{
+
+}
+
+export function passive_final_calc()
+{
+
+}
+
+export function recycle_final_calc()
+{
+
+}
+
+
